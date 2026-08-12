5f27ff735b068e1a23db77fbc405208d6d17164b
MathildeBertrand <b.mathibert@gmail.com>
2026-08-12 10:29:29 +0200

    Create xenome_launch.sh

---
+++ PDX/01_Xenome/xenome_launch.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+########################################################################
+## Script to launch Xenome
+## using: sbatch launch.sh
+########################################################################
+
+## JOB PARAMETERS >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+#SBATCH --job-name=xenome
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=1G
+#SBATCH --partition=longq
+#SBATCH --output=job_%j.out
+#SBATCH --error=job_%j.err
+
+source /mnt/beegfs02/software/recherche/miniconda/25.1.1/etc/profile.d/conda.sh
+conda activate /home/ma_bertrand/environnements_conda/xenome_env
+module load snakemake/7.32.4
+export LD_LIBRARY_PATH=/home/ma_bertrand/environnements_conda/xenome_env/lib:$LD_LIBRARY_PATH
+export TMPDIR=/mnt/beegfs01/scratch/ma_bertrand/tmp
+
+#parameters
+path_to_configfile="config_xenome.json"
+
+
+
+#launch
+snakemake --profile /mnt/beegfs01/scratch/ma_bertrand/pipelines_save_bk/profiles/slurm \
+-s /home/ma_bertrand/pipelines/Xenome/snakefile \
+--default-resources "tmpdir='ma_bertrand/tmp'" \
+--configfile ${path_to_configfile}  \
+--use-conda --jobs 15
+
+conda deactivate
+
